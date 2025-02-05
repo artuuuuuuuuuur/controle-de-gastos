946e279e6755bc262085a8cb8bcbe65d87349adf
Artur de Maria <arturdemaria7@gmail.com>
2025-02-05 03:42:05 -0300

    update: charts styled

--- src/Components/Pages/Graficos/Chart.jsx
+++ src/Components/Pages/Graficos/Chart.jsx
@@ -3,17 +3,17 @@ import { Bar, Pie } from "react-chartjs-2";
 
 function Chart({ type, data, options, optionsBar, category }) {
   return (
-    <>
-      <>
-        <h2>Por {category}</h2>
+    <div className="flex flex-col w-full max-w-2xl sm:max-w-md p-4">
+      <h2 className="font-bold text-lg text-center mb-4">Por {category}</h2>
 
+      <div className="w-full min-h-[300px]">
         {type ? (
           <Pie data={data} options={options} />
         ) : (
-          <Bar options={optionsBar} data={data} />
+          <Bar data={data} options={optionsBar} />
         )}
-      </>
-    </>
+      </div>
+    </div>
   );
 }
 
--- src/Components/Pages/Graficos/Graficos.jsx
+++ src/Components/Pages/Graficos/Graficos.jsx
@@ -53,33 +53,37 @@ function Graficos({ gastosArray }) {
     <div className="h-full max-h-full flex flex-col">
       <NavBar currentPage={"grafico"} />
       <div className="bg-white grow overflow-auto rounded-3xl p-5">
-        <div className="flex">
+        <div className="flex justify-between flex-wrap ">
           <h1 className="text-3xl font-bold">Seus gastos</h1>
-          Gráfico de Colunas
-          <Switch onClick={() => setType(!type)} defaultChecked />
-          Gráfico de Pizza
+          <span className="text-lg">
+            Gráfico de Colunas
+            <Switch onClick={() => setType(!type)} defaultChecked />
+            Gráfico de Pizza
+          </span>
         </div>
-        <Chart
-          data={categoriasData}
-          options={options}
-          type={type}
-          category={"categoria"}
-          optionsBar={options[1]}
-        />
-        <Chart
-          data={nomesData}
-          options={options}
-          type={type}
-          category={"nomes"}
-          optionsBar={options[1]}
-        />
-        <Chart
-          data={mesesData}
-          options={options}
-          type={type}
-          category={"mês"}
-          optionsBar={options[1]}
-        />
+        <div className="flex flex-wrap gap-5 justify-center">
+          <Chart
+            data={categoriasData}
+            options={options}
+            type={type}
+            category={"categoria"}
+            optionsBar={options[1]}
+          />
+          <Chart
+            data={nomesData}
+            options={options}
+            type={type}
+            category={"nomes"}
+            optionsBar={options[1]}
+          />
+          <Chart
+            data={mesesData}
+            options={options}
+            type={type}
+            category={"mês"}
+            optionsBar={options[1]}
+          />
+        </div>
       </div>
     </div>
   );
